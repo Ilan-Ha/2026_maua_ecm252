e952836f2f2ac0a506438bcdba909949412e0581
Ilan Hameiry <Ilan.hame@gmail.com>
2026-03-07 10:40:35 -0300

    learn(ap03-react): encerra a apostila

--- ap003_react_jsx_componentes/src/App.jsx
+++ ap003_react_jsx_componentes/src/App.jsx
@@ -1,14 +1,29 @@
+import './styles.css'
+
 const App = () => {
+
+  const estiloBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', borderStyle: 'hidden', color: 'white', width: '100%', borderRadius: 8}
+
+  const textoDoRotulo = 'Nome:'
+  
+  const obterTextoDoBotao = () => 'Enviar'
+
+  const aoClicar = () => alert('Clicou')
+
   return <div style={{margin: 'auto', width: 576, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
-    <label htmlFor="campoNome" style={{display: 'block', marginBottom: 8}}>
-      Nome:
+    <label className="rotulo" htmlFor="campoNome" style={{display: 'block', marginBottom: 8}}>
+      {textoDoRotulo}
     </label>
     {/* input#campoNome*/}
     <input type="text" id="campoNome" style={{paddingTop: 8, paddingBottom: 8, width: '100%', borderStyle: 'hidden', outline: 'none', borderRadius: 8}}/>
 
     {/* button(Enviar) */}
-    <button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', borderStyle: 'hidden', color: 'white', width: '100%', borderRadius: 8}}>
-      Enviar
+    <button 
+      onClick={aoClicar}
+      style={estiloBotao}>
+        {/*ou onClick ={() => aoClicar()} 
+          se fizer onClick = {aoClicar()} a função executa quando a pagina carrega ao invés de ficar vinculada ao botão*/}
+      {obterTextoDoBotao()}
       </button>
   </div>
 }
